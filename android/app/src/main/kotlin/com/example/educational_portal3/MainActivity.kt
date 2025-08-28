5c9be3f533d27fc113e01758aa1642779e50b9ec
work-piyush006 <work.piyush006@gmail.com>
2025-08-28 14:08:54 +0530

    Update and rename Main activity.java to MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/educational_portal3/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.educational_portal3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // V2 embedding automatically handles FlutterEngine
+}
